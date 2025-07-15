766d90e8aa8697fb281933048f7ba793b0359ea7
Dmytro <tsaplincinema@gmail.com>
2025-07-15 21:33:01 +1000

    Fix: export default initMongoConnection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import initMongoConnection from './db/initMongoConnection.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 const startApp = async () => {
   await initMongoConnection();
