ef1061642f5e6897c5462e44bb5756066d42edac
Dmytro <tsaplincinema@gmail.com>
2025-08-01 20:22:12 +1000

    fix: move authRouter before error handlers

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import contactsRouter from './routes/contacts.routes.js';
+import authRouter from './routes/auth.routes.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
-import authRouter from './routes/auth.routes.js';
 
 export const setupServer = () => {
   const app = express();
@@ -13,12 +13,11 @@ export const setupServer = () => {
   app.use(pino());
   app.use(express.json());
 
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
   app.use(notFoundHandler);
   app.use(errorHandler);
-
-  app.use('/auth', authRouter);
 
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
