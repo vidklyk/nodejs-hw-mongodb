371cbce98d0745ff6b02b02e8efcfb5cc501984a
Dmytro <tsaplincinema@gmail.com>
2025-08-05 16:15:58 +1000

    fix: disable secure cookie for Postman testing

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,7 +46,6 @@ export const refreshSessionController = async (req, res) => {
     .cookie('refreshToken', newRefreshToken, {
       httpOnly: true,
       sameSite: 'strict',
-      secure: false,
       maxAge: 30 * 24 * 60 * 60 * 1000,
     })
     .status(200)
